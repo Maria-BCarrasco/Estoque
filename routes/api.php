207d93a53b943c318ec5c5f2578a83286a2c9aed
Maria-BCarrasco <mariaedubcarrasco@gmail.com>
2026-01-22 14:06:24 -0300

    Implementado CRUD de produtos

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/produto', [ProdutoController::class, 'index']);
+Route::post('/produto', [ProdutoController::class, 'store']);
+Route::put('/produto', [ProdutoController::class, 'update']);
+Route::delete('/produto/{id}', [ProdutoController::class, 'delete']);
+Route::get('/produto/{id}', [ProdutoController::class, 'show']);
